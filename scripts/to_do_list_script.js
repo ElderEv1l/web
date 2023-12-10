b2c9d31664f3d060dabc7d3965b4fdaf1ca5c6c7
ElderEv1l <kiryanov.gleb39@gmail.com>
2023-12-10 22:27:20 +0300

    feat: fixed lab-5

--- scripts/to_do_list_script.js
+++ scripts/to_do_list_script.js
@@ -9,9 +9,22 @@ document.addEventListener('DOMContentLoaded', function () {
 function addTask(task){
     const listContainer = document.getElementById('task_list');
     const template = document.getElementById('list_item_template');
-    const clone = document.importNode(template.content, true)
+    const clone = document.importNode(template.content, true);
+    const items = listContainer.querySelectorAll('.task_title');
 
-    clone.querySelector('span').textContent = task;
+    let exists = false;
+    items.forEach((item) => {
+        if (item.textContent === task.trim()) {
+            exists = true;
+        }
+    });
+
+    if (task.trim() === '' || exists === true) {
+        alert('Incorrect input');
+        return;
+    }
+
+    clone.querySelector('span').textContent = task.trim();
     listContainer.appendChild(clone);
 }
 
@@ -35,7 +48,7 @@ function doneTask(event) {
 }
 
 function getFormFeedback() {
-    let form = document.getElementById('to_do_list_form')
+    let form = document.getElementById('to_do_list_form');
 
     if (form) {
         form.addEventListener('submit', function (event) {
@@ -52,18 +65,39 @@ function getFormFeedback() {
 }
 
 function getButtonsFeedback() {
-    let listContainer = document.getElementById("task_list")
+    let listContainer = document.getElementById("task_list");
 
-    listContainer.addEventListener('click', deleteTask)
-    listContainer.addEventListener('click', doneTask)
+    listContainer.addEventListener('click', deleteTask);
+    listContainer.addEventListener('click', doneTask);
 }
 
 function saveData() {
-    let listContainer = document.getElementById("task_list")
-    localStorage.setItem('data', listContainer.innerHTML)
+    let listContainer = document.getElementById("task_list");
+    let items = listContainer.querySelectorAll('.task_title');
+
+    let tasks =[];
+
+    items.forEach((item) => {
+        tasks.push({name: item.textContent, status: item.classList.contains('task_title--done')})
+    });
+
+    localStorage.setItem('data', JSON.stringify(tasks));
 }
 
 function loadFromLocal() {
-    let listContainer = document.getElementById("task_list")
-    listContainer.innerHTML = localStorage.getItem('data')
+    let listContainer = document.getElementById("task_list");
+    listContainer.innerHTML = '';
+    const template = document.getElementById('list_item_template');
+
+    let tasks = JSON.parse(localStorage.getItem('data'));
+    tasks.forEach((task) => {
+        let clone = document.importNode(template.content, true);
+
+        clone.querySelector('span').textContent = task.name;
+        if (task.status === true) {
+            clone.querySelector('span').classList.toggle('task_title--done');
+        }
+
+        listContainer.appendChild(clone);
+    });
 }
